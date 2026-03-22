132faae9718af0a2651af1e0a38725ec34789569
CaptainEtrain <ethanbcampbell@gmail.com>
2026-03-22 12:45:34 -0400

    test1

--- puzzle11.java
+++ puzzle11.java
@@ -2,6 +2,9 @@ public class puzzle11 {
     public static void main(String[] args) {
         String s = "abc";
         System.out.println(s);
+        int[] x = { 1, 2, 2
+
+        };
     }
 
 }
